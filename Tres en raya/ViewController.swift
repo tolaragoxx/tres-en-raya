b4cbf6cc3f14fe2293ae87695d7ff4e81a70db47
Gonzalo Toledo <tolaragoxx@gmail.com>
2016-01-25 17:49:39 -0500

    Initial Commit

---
+++ Tres en raya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tres en raya
+//
+//  Created by Gonzalo on 25/01/16.
+//  Copyright © 2016 doapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
