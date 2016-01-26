802c7666dbfaa7ad17ef3e8ce41201ffffbd230e
Gonzalo Toledo <tolaragoxx@gmail.com>
2016-01-25 19:00:45 -0500

    me quede en capitulo 52

--- Tres en raya/ViewController.swift
+++ Tres en raya/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(sender: AnyObject) {
+        let image = UIImage(named: "x.png")
+        sender.setImage(image, forState: .Normal)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
